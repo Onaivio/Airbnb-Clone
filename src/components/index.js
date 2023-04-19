38eb745c2a8948e69a87bf1f2a5ec5393313e319
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-04-19 17:13:46 +0100

    airbnb-clone

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Listings from './Listings'
+import ListingCard from './ListingCard'
+import NoResult from './NoResult'
+import Map from './Map.jsx'
+import MapPoint from './MapPoint.jsx'
+import OverlayContainer from './OverlayContainer.jsx'
+import ApartmentCard from './ApartmentCard.jsx'
+import ApartmentPoint from './ApartmentPoint.jsx'
+
+
+export {
+    Navbar,
+    Listings,
+    Footer,
+    ListingCard,
+    NoResult,
+    Map,
+    MapPoint,
+    OverlayContainer,
+    ApartmentCard,
+    ApartmentPoint
+}
